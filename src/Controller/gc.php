f6de73dd534ab11eb2b60be9095fd5012ed46d93
Néstor Cabello Herrero <nes@MBP-de-Nestor-2.lan>
2021-10-18 22:13:42 +0200

    new BigQueryClient

--- src/Controller/gc.php
+++ src/Controller/gc.php
@@ -5,15 +5,46 @@ use App\Service\GoogleCloud;
 use Google\Cloud\BigQuery\BigQueryClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
-
+use Google\Cloud\Core\ExponentialBackoff;
 
 class gc //extends GoogleCloud
 {
     public function get(): Response
     {
-       
+
+        $projectId = 'aerobic-acronym-329211';
+        $serviceAccountPath = './../Service/key.json';
+        $query = 'SELECT id, view_count FROM `bigquery-public-data.stackoverflow.posts_questions`';
+
+        $bigQuery = new BigQueryClient([
+            'projectId' => $projectId,
+            'keyFilePath' => $serviceAccountPath,
+        ]);
+
+        $jobConfig = $bigQuery->query($query);
+        $job = $bigQuery->startQuery($jobConfig);
+
+        $backoff = new ExponentialBackoff(10);
+        $backoff->execute(function () use ($job) {
+            print('Waiting for job to complete' . PHP_EOL);
+            $job->reload();
+            if (!$job->isComplete()) {
+                throw new Exception('Job has not yet completed', 500);
+            }
+        });
+      
+        $queryResults = $job->queryResults();
+
+        $i = 0;
+        foreach ($queryResults as $row) {
+            printf('--- Row %s ---' . PHP_EOL, ++$i);
+            foreach ($row as $column => $value) {
+                printf('%s: %s' . PHP_EOL, $column, json_encode($value));
+            }
+        }
+        printf('Found %s row(s)' . PHP_EOL, $i);
         echo "<script>console.log('Get ')</script>";
-        
+       
         return new Response (
             '<html><body>Query:'.$queryResults.'</body></html>'
         );
--- src/Service/GoogleCloud.php
+++ src/Service/GoogleCloud.php
@@ -1,62 +1,52 @@
 <?php
 namespace Google\Cloud\Samples\Auth;
-// require __DIR__ .'./../../vendor/autoload.php';
-// use Google\Cloud\BigQuery\BigQueryClient;
-// use Google\Cloud\Storage\StorageClient;
-// Imports the Cloud Storage client library.
-use Google\Cloud\Storage\StorageClient;
 
-/**
- * Authenticate to a cloud client library using a service account explicitly.
- *
- * @param string $projectId           The Google project ID.
- * @param string $serviceAccountPath  Path to service account credentials JSON.
- */
-$projectId = 'aerobic-acronym-329211';
-$serviceAccountPath = './key2.json';
+require __DIR__ .'./../../vendor/autoload.php';
+
+use Google\Cloud\Storage\StorageClient;
 
 function auth_cloud_explicit($projectId, $serviceAccountPath)
 {
-    # Explicitly use service account credentials by specifying the private key
-    # file.
-    $config = [
-        'keyFilePath' => $serviceAccountPath,
-        'projectId' => $projectId,
-    ];
-    $storage = new StorageClient($config);
+  # Explicitly use service account credentials by specifying the private key
+  # file.
+  $config = [
+      'keyFilePath' => $serviceAccountPath,
+      'projectId' => $projectId,
+  ];
+  $storage = new StorageClient($config);
 
-    # Make an authenticated API request (listing storage buckets)
-    foreach ($storage->buckets() as $bucket) {
-        printf('Bucket: %s' . PHP_EOL, $bucket->name());
+  # Make an authenticated API request (listing storage buckets)
+  foreach ($storage->buckets() as $bucket) {
+      printf('Bucket: %s' . PHP_EOL, $bucket->name());
     }
-
-echo "<script>console.log('Service')</script>";
-
-//Query
-
-  $query = <<<ENDSQL
-  SELECT
-    CONCAT(
-      'https://stackoverflow.com/questions/',
-      CAST(id as STRING)) as url,
-    view_count
-  FROM `bigquery-public-data.stackoverflow.posts_questions`
-  WHERE tags like '%google-bigquery%'
-  ORDER BY view_count DESC
-  LIMIT 10;
-  ENDSQL;
-  $queryJobConfig = $bigQuery->query($query);
-  $queryResults = $bigQuery->runQuery($queryJobConfig);
-
-  if ($queryResults->isComplete()) {
-      $i = 0;
-      $rows = $queryResults->rows();
-      foreach ($rows as $row) {
-          printf('--- Row %s ---' . PHP_EOL, ++$i);
-          printf('url: %s, %s views' . PHP_EOL, $row['url'], $row['view_count']);
-      }
-      printf('Found %s row(s)' . PHP_EOL, $i);
-  } else {
-      throw new Exception('Query failed');
-  }
 }
+
+// //Query
+
+// $query = <<<ENDSQL
+// SELECT
+//   CONCAT(
+//     'https://stackoverflow.com/questions/',
+//     CAST(id as STRING)) as url,
+//   view_count
+// FROM `bigquery-public-data.stackoverflow.posts_questions`
+// WHERE tags like '%google-bigquery%'
+// ORDER BY view_count DESC
+// LIMIT 10;
+// ENDSQL;
+// $queryJobConfig = $bigQuery->query($query);
+// $queryResults = $bigQuery->runQuery($queryJobConfig);
+
+// if ($queryResults->isComplete()) {
+//     $i = 0;
+//     $rows = $queryResults->rows();
+//     foreach ($rows as $row) {
+//         printf('--- Row %s ---' . PHP_EOL, ++$i);
+//         printf('url: %s, %s views' . PHP_EOL, $row['url'], $row['view_count']);
+//     }
+//     printf('Found %s row(s)' . PHP_EOL, $i);
+// } else {
+//     throw new Exception('Query failed');
+// }
+
+echo "<script>console.log('Service ')</script>";
